7bd18e282cfc87c8a939925edd385c5fff28de8c
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-17 00:06:09 +0800

    vboxware: Enable EPPE check for AOSP product makefile
    Change-Id: I025c3b59787cebc1a3da0566aa66c9fe928e6d38

--- aosp_vboxware.mk
+++ aosp_vboxware.mk
@@ -11,6 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from device
 $(call inherit-product, device/virt/vboxware/device.mk)
 
+$(call enforce-product-packages-exist,\
+    android.hardware.health@2.0-impl-default.recovery \
+    DeviceDiagnostics \
+    product_manifest.xml)
+
 PRODUCT_NAME := aosp_vboxware
 PRODUCT_DEVICE := vboxware
 PRODUCT_BRAND := vboxware
