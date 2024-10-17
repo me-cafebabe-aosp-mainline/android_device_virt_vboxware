7eafd7939df2dd31e1761c6f7bc2ab59db62fc5b
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-18 03:38:01 +0800

    vboxware: fstab: A-only / A/B genrule
    Change-Id: I6de4e8d049f3de72c28a68e4c9459b60b703ed99

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,13 @@ TARGET_GRUB_INSTALL_CONFIG := $(DEVICE_PATH)/bootmgr/grub/grub-install.cfg
 TARGET_REFIND_BOOT_CONFIG := $(DEVICE_PATH)/bootmgr/rEFInd/refind-boot.conf
 TARGET_REFIND_INSTALL_CONFIG := $(DEVICE_PATH)/bootmgr/rEFInd/refind-install.conf
 
+# Fstab
+ifeq ($(AB_OTA_UPDATER),true)
+$(call soong_config_set,VBOXWARE_FSTAB,PARTITION_SCHEME,ab)
+else
+$(call soong_config_set,VBOXWARE_FSTAB,PARTITION_SCHEME,a)
+endif
+
 # GRUB
 TARGET_GRUB_ARCH := x86_64-efi
 
@@ -49,7 +56,7 @@ BOARD_VENDOR_KERNEL_MODULES := \
 endif
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/configs/fstab/fstab.vboxware
+TARGET_RECOVERY_FSTAB_GENRULE := gen_fstab_vboxware
 
 # SELinux
 BOARD_VENDOR_SEPOLICY_DIRS += \
