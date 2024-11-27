ca109aa4c65c4c2b0e93454ab19866c6189e63bf
Yumi Yukimura <me.cafebabe@gmail.com>
2024-11-28 01:08:00 +0800

    vboxware: bootmgr: grub: install: Abort on unsupported machine
    * Prevents people from trying to run it on other random machines
    
    Change-Id: I567fad7584e1273a2b958b833d6df37799df4782

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,7 @@ TARGET_ARCH_VARIANT := sandybridge
 TARGET_EFI_BOOT_SCRIPTS := $(wildcard $(DEVICE_PATH)/bootmgr/linux_efi_stub/boot/*.nsh)
 TARGET_EFI_INSTALL_SCRIPTS := $(wildcard $(DEVICE_PATH)/bootmgr/linux_efi_stub/install/*.nsh)
 TARGET_GRUB_BOOT_CONFIGS += $(DEVICE_PATH)/bootmgr/grub/grub-boot.cfg
+TARGET_GRUB_INSTALL_CONFIGS += $(DEVICE_PATH)/bootmgr/grub/grub-install.cfg
 TARGET_REFIND_BOOT_CONFIG := $(DEVICE_PATH)/bootmgr/rEFInd/refind-boot.conf
 TARGET_REFIND_INSTALL_CONFIG := $(DEVICE_PATH)/bootmgr/rEFInd/refind-install.conf
 
